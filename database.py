e6601ee18073757c06282ea5ad79a57f4ab966f5
Tomislav Maslač <tm48782@fer.hr>
2020-08-24 18:19:44 +0200

    Added database file

--- database.py
+++ database.py
@@ -4,6 +4,8 @@ import uuid
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask import Flask
+from sqlalchemy import Table
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 
@@ -17,6 +19,15 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+artist_genres = Table('artist_genres', db.metadata,
+                      db.Column('artist_id', UUID(as_uuid=True), db.ForeignKey('artists.id')),
+                      db.Column('genre_id', UUID(as_uuid=True), db.ForeignKey('genres.id')))
+
+similar_artists = Table('similar_artists', db.metadata,
+                        db.Column('artist1_id', UUID(as_uuid=True), db.ForeignKey('artists.id')),
+                        db.Column('artist2_id', UUID(as_uuid=True), db.ForeignKey('artists.id')),
+                        db.Column('match', db.NUMERIC, default=0))
+
 
 class Artist(db.Model):
     __tablename__ = 'artists'
@@ -24,6 +35,17 @@ class Artist(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     name = db.Column(db.String, unique=False, nullable=False)
     image_url = db.Column(db.String, unique=False, nullable=True)
+    mb_id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=True)
+    bio = db.Column(db.TEXT, unique=False, nullable=True)
+
+    children = relationship("Child", secondary=artist_genres)
+
+
+class Genre(db.Model):
+    __tablename__ = 'genres'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
 
 
 if __name__ == '__main__':
