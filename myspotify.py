eb7f1694cc98fb2bdfd69ec9e35b754427e37819
tomislav-95 <tomislav.maslac95@gmail.com>
2020-09-05 18:49:33 +0200

    Update .gitignore

--- myspotify.py
+++ myspotify.py
@@ -1,5 +1,5 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="c1ebf6c270e7494190ade10285721a01",
-                                                           client_secret="878c4ab18b6346d384ce312224aa98c6"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="",
+                                                           client_secret=""))
